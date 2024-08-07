34909ec384550ee197cc8e9556a6a35d60e62ca5
Mauricio Cox <coxmau77@gmail.com>
2024-08-07 18:12:55 -0300

    Conceptos Básicos de Programación

---
+++ python_00_basic/00_sintaxis.py
@@ -0,0 +1,32 @@
+# Sintáxis básica
+print("Hola mundo!")
+
+# Declaración e inicializacion de variable
+userName = "MAU"
+message = "Hi"
+
+print(userName)
+print(message)
+
+# Nombramientos de variables
+# _variable = value
+# _variable_ = value
+# variable_ = value
+# variable = value
+# variable_00 = value
+# variable00 = value
+# lowerCamelCase = value
+# upperCamelCase = value
+# snake_case = value
+# KOBOLCASE = value
+
+# Ejercicio mis primeros print()
+# En este ejercicio, tu desafío es utilizar la función print para imprimir tres mensajes en la sección Console. Para ello, debes utilizar la función print tres veces en el siguiente orden con los siguientes mensajes:
+
+# "Te doy la bienvenida al Coding Playground"
+# "Mi primer print"
+# "30"
+
+print("Te doy la bienvenida al Coding Playground")
+print("Mi primer print")
+print("30")
